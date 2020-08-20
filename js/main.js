bce93d90dc47755a06cdfc3b92b6e5597dad83d9
Olexandr <alexandr90h@gmail.com>
2020-08-20 23:32:07 +0300

    20.08.2020

---
+++ js/main.js
@@ -0,0 +1,58 @@
+class Car {
+  // Write code under this line
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
+  }
+  constructor({ maxSpeed, speed=0, isOn = false, distance=0, price }) {
+    this.maxSpeed = maxSpeed;
+    this.speed = speed;
+    this.isOn = isOn;
+    this.distance = distance;
+    this._price = price;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (this.speed + value < this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+  decelerate(value) {
+    if (this.speed - value > 0) {
+      this.speed -= value;
+    }
+  }
+  drive(hours) {
+    if (this.isOn === true) {
+      this.distance = this.speed * hours;
+    }
+  }
+}
+const mustang = new Car({maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
