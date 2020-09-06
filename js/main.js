1d818850f35c922b33f52dffd0875cd7d379b2a2
Olexandr <alexandr90h@gmail.com>
2020-09-06 23:12:30 +0300

    06.09.2020

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const formConteiner = document.querySelector('#form-conteiner');
+const numberInput = document.querySelector('#number-input');
+const minInput = document.querySelector('#min-input');
+const maxInput = document.querySelector('#max-input');
+const btn = document.querySelector('#button-input');
+
+console.log(document.querySelector('table'));
+const clickBtnFun = function () {
+  const obj = randomFunction(
+    Number(numberInput.value),
+    Number(minInput.value),
+    Number(maxInput.value),
+  );
+
+  const tableBox = document.createElement('table');
+  const summBox = document.createElement('p');
+  summBox.classList.add('summ-box');
+  if (document.querySelector('table') === null) {
+    formConteiner.after(tableBox);
+    tableBox.after(summBox);
+  } else {
+    document.querySelector('table').replaceWith(tableBox);
+    document.querySelector(".summ-box").replaceWith(summBox);
+  }
+
+  obj.arr.forEach((num, idx) => {
+    const tabLine = document.createElement('tr');
+    const tabCell = document.createElement('td');
+    const tabCellNum = document.createElement('td');
+    tabCellNum.style.backgroundColor = 'red';
+    tableBox.appendChild(tabLine).append(tabCell, tabCellNum);
+    tabCell.textContent = idx + 1;
+    tabCellNum.textContent = num;
+  });
+  summBox.textContent = obj.sun;
+};
+btn.addEventListener('click', clickBtnFun);
--- js/random-number.js
+++ js/random-number.js
@@ -1,68 +1,62 @@
-const number = prompt('Кількість чисел:');
-//---------------------
-const rangeMin = 2900;
-const rangeMax = 3400;
-let maxSumm = getRandomInt(rangeMin, rangeMax);
-let randNum = [];
-let array = 0;
-let a = 20;
-let part = Math.floor(maxSumm / number);
-//----------------------
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення
-}
-const summArray = function (arr) {
-  let summArr = 0;
+const randomFunction = function (number, rangeMin, rangeMax) {
+  //---------------------
+  // number = number === 0 ? 1 : number;
+  let maxSumm = getRandomInt(rangeMin, rangeMax);
+  let randNum = [];
+  let array = 0;
+  let a = 20;
+  let part = Math.floor(maxSumm / number);
+  //----------------------
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення
+  }
+  const summArray = function (arr) {
+    let summArr = 0;
+    for (let i = 0; i < number; i += 1) {
+      summArr += arr[i];
+    }
+    return summArr;
+  };
+  const transferDifference = function (arr) {
+    let transDiff = getRandomInt(0, number - 1);
+    arr.forEach(function (num, idx) {
+      if (idx === transDiff) {
+        arr[idx] = Math.floor(num / 2);
+        arr.splice(idx + 1, 1, arr[idx + 1] + Math.floor(num / 2));
+      }
+    });
+    return arr;
+  };
+
   for (let i = 0; i < number; i += 1) {
-    summArr += arr[i];
+    randNum.push(getRandomInt(a, part));
   }
-  return summArr;
-};
-const transferDifference = function (arr) {
-  let transDiff = getRandomInt(0, number - 1);
-  arr.forEach(function (num, idx) {
-    if (idx === transDiff) {
-      arr[idx] = Math.floor(num / 2);
-      arr.splice(idx + 1, 1, arr[idx + 1] + Math.floor(num / 2));
+  array = summArray(randNum);
+  while (array < rangeMin || array > rangeMax) {
+    if (array < rangeMin) {
+      part = (rangeMin - array) / number;
+      array = 0;
+      for (let i = 0; i < number; i += 1) {
+        randNum[i] += getRandomInt(a, part);
+      }
+      array = summArray(randNum);
+    } else if (array > rangeMax) {
+      part = (array - rangeMax) / number;
+      array = 0;
+      for (let i = 0; i < number; i += 1) {
+        randNum[i] -= getRandomInt(a, randNum[i]);
+      }
+      array = summArray(randNum);
     }
-  });
-  return arr;
-};
-
-// console.log(`Початкова сума: ${maxSumm}`);
-// console.log(`Частина: ${part}`);
-for (let i = 0; i < number; i += 1) {
-  randNum.push(getRandomInt(a, part));
-}
-array = summArray(randNum);
-// console.log(`Сума= ${array}`);
-
-// console.log(`randNum (до) = ${randNum}`);
-while (array < rangeMin || array > rangeMax) {
-  if (array < rangeMin) {
-    part = (rangeMin - array) / number;
-    array = 0;
-    for (let i = 0; i < number; i += 1) {
-      randNum[i] += getRandomInt(a, part);
-    }
-    array = summArray(randNum);
-  } else if (array > rangeMax) {
-    part = (array - rangeMax) / number;
-    array = 0;
-    for (let i = 0; i < number; i += 1) {
-      randNum[i] -= getRandomInt(a, randNum[i]);
-    }
-    array = summArray(randNum);
   }
-}
-// randNum.forEach((nam, idx) => console.log(`#${idx} - ${nam}`));
-// console.log(`transferDifference=${transDiff}`);
-for (let i = 0; i < number; i++) {
-  randNum = transferDifference(randNum);
-}
+  for (let i = 0; i < number; i++) {
+    randNum = transferDifference(randNum);
+  }
 
-// console.log(`randNum (після)`);
-randNum.forEach((nam, idx) => console.log(`#${idx} - ${nam}`));
-console.log(`Сума = ${array}`);
+  // randNum.forEach((nam, idx) => console.log(`#${idx} - ${nam}`));
+  // console.log(`Сума = ${array}`);
+  const obj = { arr: randNum, sun: array };
+  return obj;
+};
